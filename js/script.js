9249dcc6484af493271b20de311dcc28c218aece
mobinazaredoost <mobinazaredoost@gmail.com>
2026-07-20 17:04:51 +0330

    JS HEADER SCROLL EFFECT

--- js/script.js
+++ js/script.js
@@ -17,3 +17,50 @@ document.querySelectorAll(".nav-links a").forEach(link=>{
         '<i class="fa-solid fa-bars"></i>';
     });
 });
+
+// HEADER SCROLL EFFECT
+const header = document.querySelector("header");
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }
+    else{
+        header.classList.remove("scrolled");
+    }
+});
+
+// REVEAL SECTIONS
+const revealElements =
+document.querySelectorAll(
+".card, .feature, .stat, .review, .cta"
+);
+const revealObserver =
+new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:.15
+});
+revealElements.forEach(el=>{
+    el.classList.add("hidden");
+    revealObserver.observe(el);
+});
+
+        const target =
+        document.querySelector(
+        this.getAttribute("href")
+        );
+        if(target){
+            e.preventDefault();
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+        }
+    });
+
+});
+
+
