ff41b3a5cac29c62c78aa6b5f930a436f6885d8a
mobinazaredoost <mobinazaredoost@gmail.com>
2026-07-23 01:00:48 +0330

    all js page login

--- js/auth.js
+++ js/auth.js
@@ -14,6 +14,43 @@ if(togglePassword){
         }
     });
 }
+// LOGIN VALIDATION
+const loginForm = document.querySelector("#loginForm");
+if(loginForm){
+loginForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const email = document.querySelector("#email").value.trim();
+    const password = document.querySelector("#password").value.trim();
+    if(email === "" || password === ""){
+        showMessage(
+            "لطفاً تمام فیلدها را پر کنید",
+            "error"
+        );
+        return;
+    }
+    if(password.length < 6){
+        showMessage(
+            "رمز عبور باید حداقل ۶ کاراکتر باشد",
+            "error"
+        );
+        return;
+    }
+    showMessage(
+        "ورود با موفقیت انجام شد",
+        "success"
+    );
+});
+}
+// MESSAGE FUNCTION
+function showMessage(text,type){
+    let message = document.querySelector(".auth-message");
+    if(!message){
+        message = document.createElement("div");
+        message.className="auth-message";
+        document.querySelector(".auth-box")
+        .appendChild(message);
+    }
+    message.innerText=text;
     message.className =
     "auth-message " + type;
     setTimeout(()=>{
