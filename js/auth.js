752aef8321657faa529998b33ee3b9a5c45d9b3d
mobinazaredoost <mobinazaredoost@gmail.com>
2026-07-23 01:21:02 +0330

    update js page login and sing in

--- js/auth.js
+++ js/auth.js
@@ -1,19 +1,5 @@
-// start SHOW / HIDE PASSWORD
-const togglePassword = document.querySelector(".toggle-password");
-const passwordInput = document.querySelector("#password");
-if(togglePassword){
-    togglePassword.addEventListener("click",()=>{
-        if(passwordInput.type === "password"){
-            passwordInput.type = "text";
-            togglePassword.classList.remove("fa-eye");
-            togglePassword.classList.add("fa-eye-slash");
-        }else{
-            passwordInput.type = "password";
-            togglePassword.classList.remove("fa-eye-slash");
-            togglePassword.classList.add("fa-eye");
         }
     });
-}
 // LOGIN VALIDATION
 const loginForm = document.querySelector("#loginForm");
 if(loginForm){
