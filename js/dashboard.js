451c0b6d6576e0cf4891fc49982b9ba597988e30
mobinazaredoost <mobinazaredoost@gmail.com>
2026-08-07 21:56:17 +0330

    update js and 3d card

--- js/dashboard.js
+++ js/dashboard.js
@@ -110,3 +110,39 @@ function(e){
 });
 }
 
+
+//    CARD 3D
+const cards =
+document.querySelectorAll(
+".card, .dashboard-card"
+);
+cards.forEach(card=>{
+    card.addEventListener(
+    "mousemove",
+    e=>{
+        const rect =
+        card.getBoundingClientRect();
+        const x =
+        e.clientX - rect.left;
+        const y =
+        e.clientY - rect.top;
+        const rotateX =
+        (y - rect.height/2) / 20;
+        const rotateY =
+        (rect.width/2 - x) / 20;
+        card.style.transform =
+        `
+        perspective(800px)
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        translateY(-8px)
+        `;
+    });
+    card.addEventListener(
+    "mouseleave",
+    ()=>{
+        card.style.transform =
+        "";
+    });
+});
+
